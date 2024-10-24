75f0511e652986759050a53cbafbe2d752758953
alif mahendra <nacubes20@gmail.com>
2024-10-24 12:56:56 +0700

    updating env production

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express()
+const origin = process.env.ORIGIN
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:3001',  // Your frontend URL
+  origin,  // Your frontend URL
   methods: 'POST',
 }))
 
